59e9c00838c5ab015048c835eb94d89d1e5196fd
NITHIN KUMAR <reddynithinkgr07@gmail.com>
2026-04-30 21:23:39 +0530

    Update Automatic-Pill-Remainder-System.ino

--- Automatic-Pill-Remainder-System.ino
+++ Automatic-Pill-Remainder-System.ino
@@ -1,167 +1,261 @@
-// Automatic Pill Reminder System
-// Components: Arduino Uno, Buzzer, RTC Module (DS1307/DS3231), LCD Display (16x2), 3 Push Buttons
+/*
+===========================================================
+AUTOMATIC PILL REMINDER SYSTEM
+===========================================================
+
+Components Used:
+1. Arduino Uno
+2. RTC Module (DS3231 / DS1307)
+3. 16x2 I2C LCD
+4. Buzzer
+5. 3 Push Buttons
+
+Function:
+- Displays current time
+- Allows user to set reminder hour and minute
+- Activates buzzer when reminder time matches RTC time
+- Stops buzzer when confirm button is pressed
+===========================================================
+*/
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
 
-// LCD setup
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Change address if needed
+/*---------------------------------------------------------
+  LCD Initialization
+---------------------------------------------------------*/
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// RTC setup
+/*---------------------------------------------------------
+  RTC Initialization
+---------------------------------------------------------*/
 RTC_DS3231 rtc;
 
-// Pins for buttons
-const int buttonSetHourPin = 2;    // Button to set reminder hour
-const int buttonSetMinutePin = 3;  // Button to set reminder minute
-const int buttonConfirmPin = 4;    // Button to confirm reminder time
+/*---------------------------------------------------------
+  Pin Definitions
+---------------------------------------------------------*/
+#define BUTTON_SET_HOUR    2
+#define BUTTON_SET_MINUTE  3
+#define BUTTON_CONFIRM     4
+#define BUZZER_PIN         9
 
-// Buzzer pin
-const int buzzerPin = 9;
+/*---------------------------------------------------------
+  Global Variables
+---------------------------------------------------------*/
+int reminderHour = 8;
+int reminderMinute = 0;
 
-// Variables to store pill reminder time
-int reminderHour = 8;   // default reminder hour
-int reminderMinute = 0; // default reminder minute
+unsigned char settingHour = 0;
+unsigned char settingMinute = 0;
+unsigned char reminderSet = 0;
+unsigned char alarmActive = 0;
 
-// States
-bool settingHour = false;
-bool settingMinute = false;
-bool reminderSet = false;
-bool alarmActive = false;
+/*---------------------------------------------------------
+  Function Prototypes
+---------------------------------------------------------*/
+void displayTime(DateTime now);
+void displayReminder(void);
+void checkButtons(void);
+void checkAlarm(DateTime now);
+void buzzAlarm(void);
+void printTwoDigits(int num);
 
-void setup() {
-  // Initialize serial for debugging
-  Serial.begin(9600);
+/*---------------------------------------------------------
+  Setup Function
+---------------------------------------------------------*/
+void setup()
+{
+    Serial.begin(9600);
 
-  // Initialize LCD
-  lcd.begin( 16, 2 );
-  lcd.backlight();
+    /* Initialize LCD */
+    lcd.begin(16, 2);
+    lcd.backlight();
 
-  // Initialize RTC
-  if (!rtc.begin()) {
-    lcd.print("RTC Failed!");
-    while (1);
-  }
+    /* Initialize RTC */
+    if (!rtc.begin())
+    {
+        lcd.print("RTC Failed!");
+        while (1);
+    }
 
-  if (rtc.lostPower()) {
-    // Set to a default date/time
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
+    /* If RTC lost power, set current compile time */
+    if (rtc.lostPower())
+    {
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
 
-  // Initialize buttons
-  pinMode(buttonSetHourPin, INPUT_PULLUP);
-  pinMode(buttonSetMinutePin, INPUT_PULLUP);
-  pinMode(buttonConfirmPin, INPUT_PULLUP);
+    /* Configure Button Pins */
+    pinMode(BUTTON_SET_HOUR, INPUT_PULLUP);
+    pinMode(BUTTON_SET_MINUTE, INPUT_PULLUP);
+    pinMode(BUTTON_CONFIRM, INPUT_PULLUP);
 
-  // Initialize buzzer
-  pinMode(buzzerPin, OUTPUT);
-  digitalWrite(buzzerPin, LOW);
+    /* Configure Buzzer */
+    pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(BUZZER_PIN, LOW);
 
-  // Start display
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Pill Reminder");
-  delay(2000);
-  lcd.clear();
+    /* Welcome Message */
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pill Reminder");
+    delay(2000);
+    lcd.clear();
 }
 
-void loop() {
-  DateTime now = rtc.now();
+/*---------------------------------------------------------
+  Main Loop
+---------------------------------------------------------*/
+void loop()
+{
+    DateTime now = rtc.now();
 
-  // Display current time
-  lcd.setCursor(0, 0);
-  lcd.print("Time: ");
-  printTwoDigits(now.hour());
-  lcd.print(":");
-  printTwoDigits(now.minute());
-  lcd.print(":");
-  printTwoDigits(now.second());
+    /* Display current time */
+    displayTime(now);
 
-  // Display reminder time or prompt
-  lcd.setCursor(0, 1);
-  if (reminderSet) {
-    lcd.print("Remind: ");
-    printTwoDigits(reminderHour);
+    /* Display reminder time */
+    displayReminder();
+
+    /* Read button input */
+    checkButtons();
+
+    /* Check if alarm should trigger */
+    checkAlarm(now);
+
+    /* Activate buzzer if needed */
+    if (alarmActive)
+    {
+        buzzAlarm();
+    }
+}
+
+/*---------------------------------------------------------
+  Display Current Time
+---------------------------------------------------------*/
+void displayTime(DateTime now)
+{
+    lcd.setCursor(0, 0);
+    lcd.print("Time: ");
+
+    printTwoDigits(now.hour());
     lcd.print(":");
-    printTwoDigits(reminderMinute);
-    lcd.print("  ");
-  } else {
-    lcd.print("Set Reminder  ");
-  }
 
-  // Check buttons for setting reminder
-  if (digitalRead(buttonSetHourPin) == LOW) {
-    settingHour = true;
-    settingMinute = false;
-    delay(300); // debounce
-  }
+    printTwoDigits(now.minute());
+    lcd.print(":");
 
-  if (digitalRead(buttonSetMinutePin) == LOW) {
-    if (settingHour) {
-      settingHour = false;
-      settingMinute = true;
-    }
-    delay(300); // debounce
-  }
+    printTwoDigits(now.second());
+}
 
-  if (settingHour) {
-    reminderHour++;
-    if (reminderHour > 23) reminderHour = 0;
+/*---------------------------------------------------------
+  Display Reminder Time
+---------------------------------------------------------*/
+void displayReminder(void)
+{
     lcd.setCursor(0, 1);
-    lcd.print("Set Hour:      ");
-    lcd.setCursor(10, 1);
-    printTwoDigits(reminderHour);
-    delay(300);
-  } else if (settingMinute) {
-    reminderMinute++;
-    if (reminderMinute > 59) reminderMinute = 0;
-    lcd.setCursor(0, 1);
-    lcd.print("Set Min:       ");
-    lcd.setCursor(10, 1);
-    printTwoDigits(reminderMinute);
-    delay(300);
-  }
 
-  // Confirm button sets the reminder
-  if (digitalRead(buttonConfirmPin) == LOW) {
-    reminderSet = true;
-    settingHour = false;
-    settingMinute = false;
-    delay(500); // debounce
-  }
-
-  // Check if it's time to remind
-  if (reminderSet && now.hour() == reminderHour && now.minute() == reminderMinute && now.second() == 0) {
-    alarmActive = true;
-  }
-
-  if (alarmActive) {
-    buzzAlarm();
-  }
-}
-
-void printTwoDigits(int digits) {
-  if (digits < 10) {
-    lcd.print("0");
-  }
-  lcd.print(digits);
-}
-
-void buzzAlarm() {
-  // Buzz for 10 seconds or until confirm button pressed
-  unsigned long start = millis();
-  while (millis() - start < 10000) {
-    digitalWrite(buzzerPin, HIGH);
-    delay(200);
-    digitalWrite(buzzerPin, LOW);
-    delay(200);
-
-    if (digitalRead(buttonConfirmPin) == LOW) {
-      alarmActive = false;
-      digitalWrite(buzzerPin, LOW);
-      delay(500); // debounce
-      break;
+    if (reminderSet)
+    {
+        lcd.print("Remind: ");
+        printTwoDigits(reminderHour);
+        lcd.print(":");
+        printTwoDigits(reminderMinute);
     }
-  }
+    else
+    {
+        lcd.print("Set Reminder ");
+    }
 }
 
+/*---------------------------------------------------------
+  Handle Button Presses
+---------------------------------------------------------*/
+void checkButtons(void)
+{
+    /* Set Hour Button */
+    if (digitalRead(BUTTON_SET_HOUR) == LOW)
+    {
+        reminderHour++;
+
+        if (reminderHour > 23)
+            reminderHour = 0;
+
+        settingHour = 1;
+        settingMinute = 0;
+
+        delay(300);
+    }
+
+    /* Set Minute Button */
+    if (digitalRead(BUTTON_SET_MINUTE) == LOW)
+    {
+        reminderMinute++;
+
+        if (reminderMinute > 59)
+            reminderMinute = 0;
+
+        settingHour = 0;
+        settingMinute = 1;
+
+        delay(300);
+    }
+
+    /* Confirm Button */
+    if (digitalRead(BUTTON_CONFIRM) == LOW)
+    {
+        reminderSet = 1;
+        settingHour = 0;
+        settingMinute = 0;
+
+        delay(500);
+    }
+}
+
+/*---------------------------------------------------------
+  Check Alarm Condition
+---------------------------------------------------------*/
+void checkAlarm(DateTime now)
+{
+    if (reminderSet &&
+        now.hour() == reminderHour &&
+        now.minute() == reminderMinute &&
+        now.second() == 0)
+    {
+        alarmActive = 1;
+    }
+}
+
+/*---------------------------------------------------------
+  Buzzer Alarm Function
+---------------------------------------------------------*/
+void buzzAlarm(void)
+{
+    unsigned long start = millis();
+
+    while ((millis() - start) < 10000)
+    {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delay(200);
+
+        digitalWrite(BUZZER_PIN, LOW);
+        delay(200);
+
+        /* Stop alarm if confirm button pressed */
+        if (digitalRead(BUTTON_CONFIRM) == LOW)
+        {
+            alarmActive = 0;
+            digitalWrite(BUZZER_PIN, LOW);
+            delay(500);
+            break;
+        }
+    }
+}
+
+/*---------------------------------------------------------
+  Print Two Digit Number
+---------------------------------------------------------*/
+void printTwoDigits(int num)
+{
+    if (num < 10)
+        lcd.print("0");
+
+    lcd.print(num);
+}
